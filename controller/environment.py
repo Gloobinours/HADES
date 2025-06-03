d9d338bb40d12a4b80eebbae7f81a650bfd0841e
Gloobinours <kusami.louan@gmail.com>
2025-06-03 11:53:16 -0400

    Environment and agent files

---
+++ controller/environment.py
@@ -0,0 +1,12 @@
+import gym
+import numpy as np 
+
+class HoneyPotEnv(gym.Env):
+    def __init__(self):
+        super(HoneyPotEnv(), self).__init__()
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
